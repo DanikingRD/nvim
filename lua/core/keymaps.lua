86b4c8352d8cd55dea41fdf780162b7f4886fc66
DanikingRD <danikingrd@gmail.com>
2023-10-26 12:02:12 -0400

    feat: enable smarrtab

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.tabstop = 4
+vim.o.smarttab = true
 vim.o.shiftwidth = 4
 vim.o.cursorline = true
 vim.o.autoindent = true
